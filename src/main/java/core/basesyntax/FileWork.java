f300224040fc2f43f14be392e2b95a68399cff4c
Daniil Medvediev <medvedievdaniil300@gmail.com>
2025-08-31 14:15:54 +0200

    ..

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,33 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String fileContent;
+        try {
+            fileContent = Files.readString(Paths.get(fileName));
+        } catch (IOException e) {
+            return new String[0];
+        }
+
+        List<String> wordsStartingWithW = new ArrayList<>();
+        String[] allWords = fileContent.split("\\W+");
+        for (String word : allWords) {
+            if (!word.isEmpty()) {
+                String lowerCaseWord = word.toLowerCase();
+                if (lowerCaseWord.startsWith("w")) {
+                    wordsStartingWithW.add(lowerCaseWord);
+                }
+            }
+        }
+
+        Collections.sort(wordsStartingWithW);
+        return wordsStartingWithW.toArray(new String[0]);
     }
 }
